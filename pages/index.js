b1f5fd494c6f14d9f5bdde32c8ed72c7cb48ec9b
scaryboard <lab.mm.spa@gmail.com>
2022-10-30 16:41:13 -0300

    Nuevo cambio

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Pokemones({ pokemones }) {
 
   return (
     <div>
-      <p>Pokemones</p>
+      <p>Mi app de Pokemones</p>
       <ul>
         {pokemones.map(pokemon => <Pokemon pokemon={pokemon} key={pokemon.name} />)}
       </ul>
